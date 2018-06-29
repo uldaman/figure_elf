5b24981e343740f98c66cb644c9659461e0d399e
hanxiao <zhuec@hotmail.com>
2018-06-29 20:04:41 +0800

    feat: find_pic, find_pics

--- elf/elf.py
+++ elf/elf.py
@@ -3,39 +3,61 @@ import numpy as np
 from PIL import ImageGrab
 
 
-def find_pic(image, region=None, mask=None, precision=0.9):
-    """Searchs for an image within an area
+def find_pics(images, region=None, precision=0.9, mask=None):
+    """Finds for images within an area
+    除非是要找透明图, 否则不应该使用 mask, 制作 mask 时需要消耗时间
 
-    type :
-    image : string, path to the image file
+    type:
+    images: List [string], path to the image file
     region: tupe(x, y, width, height), default is the entire screen
+    precision: float, 0 ~ 1, matching precision, default is 0.9
     mask: tupe(B, G, R), these pixels will be ignored when scanning, default is don't ignored
-    precision : float, 0 ~ 1, matching precision, default is 0.9
 
-    rtype : tupe(x, y), if don't found return None
+    rtype: Iterator [tupe(x, y)], corresponds to input one by one
     """
     edged = cv2.cvtColor(np.array(ImageGrab.grab(region)), cv2.COLOR_RGB2GRAY)
-
     if mask is None:
-        template = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+        return map(lambda image: __match(edged, image, precision), images)
     else:
-        template = cv2.imread(image, cv2.IMREAD_COLOR)
-        mask = (template[:, :, 0] != mask[0]) & (template[:, :, 1] != mask[1]) & (template[:, :, 2] != mask[2])
-        mask = mask.astype(np.uint8)
-        template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        return map(lambda image: __match_by_mask(edged, image, precision, mask), images)
 
-    res = cv2.matchTemplate(edged, template, cv2.TM_CCORR_NORMED, mask=mask)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
+def find_pic(image, region=None, precision=0.9, mask=None):
+    """Finds for an image within an area
+    除非是要找透明图, 否则不应该使用 mask, 制作 mask 时需要消耗时间
+
+    type:
+    image: string, path to the image file
+    region: tupe(x, y, width, height), default is the entire screen
+    precision: float, 0 ~ 1, matching precision, default is 0.9
+    mask: tupe(B, G, R), these pixels will be ignored when scanning, default is don't ignored
+
+    rtype: tupe(x, y), if don't found return None
+    """
+    edged = cv2.cvtColor(np.array(ImageGrab.grab(region)), cv2.COLOR_RGB2GRAY)
+    if mask is None:
+        return __match(edged, image, precision)
+    else:
+        return __match_by_mask(edged, image, precision, mask)
+
+
+def __match(edged, image, precision):
+    template = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    locations = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF_NORMED)
+    return __best_point(locations, precision)
+
+
+def __match_by_mask(edged, image, precision, mask):
+    template = cv2.imread(image, cv2.IMREAD_COLOR)
+    mask = (template[:, :, 0] != mask[0]) & (template[:, :, 1] != mask[1]) & (template[:, :, 2] != mask[2])
+    mask = mask.astype(np.uint8)
+    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    locations = cv2.matchTemplate(edged, template, cv2.TM_CCORR_NORMED, mask=mask)
+    return __best_point(locations, precision)
+
+
+def __best_point(locations, precision):
+    _, max_val, _, max_loc = cv2.minMaxLoc(locations)
     if max_val < precision:
         return None
     return max_loc
-
-
-if __name__ == '__main__':
-    for i in range(0, 10):
-        point = find_pic("elf/123.bmp", (0, 0, 500, 500), (255, 255, 255))
-        print(point)
-
-    for i in range(0, 10):
-        print(i)
