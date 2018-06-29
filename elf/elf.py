b06cd4f8ae4531b9dae530bbf18f412cc716f12a
hanxiao <hanxiao@vechain.com>
2018-06-30 01:04:59 +0800

    refactor: 根据 mask 使用 GRAY/BGR 不同的匹配方式

--- elf/elf.py
+++ elf/elf.py
@@ -5,7 +5,7 @@ from PIL import ImageGrab
 
 def find_pics(images, region=None, precision=0.9, mask=None):
     """Finds for images within an area
-    除非是要找透明图, 否则不应该使用 mask, 制作 mask 时需要消耗时间
+    除非是要找透明图, 否则不应该使用 mask
 
     type:
     images: List [string], path to the image file
@@ -15,15 +15,17 @@ def find_pics(images, region=None, precision=0.9, mask=None):
 
     rtype: Iterator [tupe(x, y)], corresponds to input one by one
     """
-    edged = cv2.cvtColor(np.array(ImageGrab.grab(region)), cv2.COLOR_RGB2GRAY)
     if mask is None:
+        edged = cv2.cvtColor(np.array(ImageGrab.grab(region)), cv2.COLOR_RGB2GRAY)
         return map(lambda image: __match(edged, image, precision), images)
+
+    edged = cv2.cvtColor(np.array(ImageGrab.grab(region)), cv2.COLOR_RGB2BGR)
     return map(lambda image: __match_by_mask(edged, image, precision, mask), images)
 
 
 def find_pic(image, region=None, precision=0.9, mask=None):
     """Finds for an image within an area
-    除非是要找透明图, 否则不应该使用 mask, 制作 mask 时需要消耗时间
+    除非是要找透明图, 否则不应该使用 mask
 
     type:
     image: string, path to the image file
@@ -33,19 +35,29 @@ def find_pic(image, region=None, precision=0.9, mask=None):
 
     rtype: tupe(x, y), if don't found return None
     """
-    edged = cv2.cvtColor(np.array(ImageGrab.grab(region)), cv2.COLOR_RGB2BGR)
     if mask is None:
+        edged = cv2.cvtColor(np.array(ImageGrab.grab(region)), cv2.COLOR_RGB2GRAY)
         return __match(edged, image, precision)
+
+    edged = cv2.cvtColor(np.array(ImageGrab.grab(region)), cv2.COLOR_RGB2BGR)
     return __match_by_mask(edged, image, precision, mask)
 
 
 def __match(edged, image, precision):
-    template = cv2.imread(image, cv2.IMREAD_COLOR)
+    """匹配图像
+
+    无遮挡匹配， 应用灰度图匹配
+    """
+    template = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
     locations = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF_NORMED)
     return __best_point(locations, precision)
 
 
 def __match_by_mask(edged, image, precision, mask):
+    """匹配图像
+
+    有遮挡匹配， 应用 BGR 匹配， 灰度图误差太大
+    """
     template = cv2.imread(image, cv2.IMREAD_COLOR)
     mask = (template[:, :, ] != mask)
     mask = mask.astype(np.uint8)
