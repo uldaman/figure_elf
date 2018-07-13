6bf6a4253e1dfd006f71c15a70eff2293fd9924c
hanxiao <hanxiao@vechain.com>
2018-07-13 09:34:48 +0800

    fix: 部分情况，window name 会为 None

--- elf/hook.py
+++ elf/hook.py
@@ -2,12 +2,12 @@ from .script import get_key_handler
 
 
 def on_key_down_event(event):
-    if event.WindowName.startswith("逆水寒"):
+    if event.WindowName and event.WindowName.startswith("逆水寒"):
         get_key_handler(event.Key)(True)
     return True
 
 
 def on_key_up_event(event):
-    if event.WindowName.startswith("逆水寒"):
+    if event.WindowName and event.WindowName.startswith("逆水寒"):
         get_key_handler(event.Key)(False)
     return True
